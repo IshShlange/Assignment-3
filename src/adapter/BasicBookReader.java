9bb5f3d088eda85a102eea4f828cce2d47d9e88c
IshShlange <starper13102006@gmail.com>
2025-09-28 18:56:50 +0500

    Add files via upload

---
+++ src/adapter/BasicBookReader.java
@@ -0,0 +1,17 @@
+package adapter;
+
+import model.Book;
+
+public class BasicBookReader implements BookReader {
+    @Override
+    public void readBook(String title, String author) {
+        System.out.println("📖 Reading physical book: '" + title + "' by " + author);
+        System.out.println("   - Turning pages");
+        System.out.println("   - Physical book experience");
+    }
+
+    @Override
+    public String getReaderType() {
+        return "Physical Book Reader";
+    }
+}
---
+++ src/factory/AudioBookFactory.java
@@ -0,0 +1,21 @@
+package factory;
+
+import model.AudioBook;
+import model.Book;
+
+public class AudioBookFactory implements LibraryFactory {
+    private final double duration;
+
+    public AudioBookFactory(double duration) {
+        this.duration = duration;
+    }
+
+    public AudioBookFactory() {
+        this(5.0);
+    }
+
+    @Override
+    public Book createBook(String title, String author) {
+        return new AudioBook(title, author, duration);
+    }
+}
